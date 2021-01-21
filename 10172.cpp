7328e9a68d44f27f6069e306a386110bc9c466a5
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2021-01-21 18:10:13 +0900

    Upload files

---
+++ 10172.cpp
@@ -0,0 +1,16 @@
+// 10172
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	return 0;
+}
---
+++ 2557.cpp
@@ -0,0 +1,10 @@
+// 2557
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+    
+    return 0;
+}
