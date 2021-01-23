9ce1c730cb2767ea0def41874d215fa2630e1523
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2021-01-24 01:13:40 +0900

    Upload files

---
+++ python/mybubble_sort.py
@@ -0,0 +1,11 @@
+a = list(map(int, input().split()))
+n = len(a)
+
+for i in range(n-1):
+    for j in range(n-1-i):
+        if a[j] > a[j+1] :
+            temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+
+print(a)
