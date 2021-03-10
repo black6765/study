1ab145f9b564a42224090673220a30d4b3b51854
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2021-03-10 13:15:41 +0900

    Update 10818.java

--- java/1차원 배열/10818.java
+++ java/1차원 배열/10818.java
@@ -11,23 +11,25 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		
+		// 값을 인덱스 번호로 사용하기 위해 최소값을 0으로 맞춰줌
 		for (int i = 0; i < N; i++) {
 			arr[Integer.parseInt(st.nextToken())+1000000]++;
 		}
 		
 		int min = 0;
 		int max = 0;
+
+		// for 문에서 0부터 인덱스 값이 증가하면서 가장 먼저 만난 인덱스가 최소값
 		for (int i = 0; i <= 2000000; i++) {
-			if (arr[i] != 0)
-			{
+			if (arr[i] != 0) {
 				min = i - 1000000;
 				break;
 			}
 		}
 		
+		// for 문에서 2000000부터 인덱스 값이 감소하면서 가장 먼저 만난 인덱스가 최대값
 		for (int i = 2000000; i >= 0; i--) {
-			if (arr[i] != 0)
-			{
+			if (arr[i] != 0) {
 				max = i - 1000000;
 				break;
 			}
