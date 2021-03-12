39a56174c7e975b86d9bff46561f857497088c85
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2021-03-12 19:34:33 +0900

    Update 1065.java

--- java/함수/1065.java
+++ java/함수/1065.java
@@ -11,7 +11,7 @@ public class Main {
 		// 1~N까지 for 문 실행
 		// 리턴 값이 0이면 한수라는 뜻으로 cnt++
 		for (int i = 1; i <= N; i++) {
-			if(func(i) == 0 ) {
+			if(func(i)) {
 				cnt++;
 			}
 		}
@@ -24,14 +24,14 @@ public class Main {
 		br.close();
 	}
 	
-	private static int func (int n) {
+	private static boolean func (int n) {
 		// 정수 n을 String으로 변경
 		// 인덱스 접근(charAt)을 통해 공차를 쉽게 구하기 위함
 		String target = String.valueOf(n);
 		
 		// 두자리 수 이하는 모두 한수
 		if (target.length() <= 2) {
-			return 0;
+			return true;
 		}
 		
 		// d는 등차수열의 공차
@@ -39,10 +39,10 @@ public class Main {
 		for (int i = 1; i < target.length() - 1; i++) {
 			// 만약 두 수의 차이가 공차와 같지 않다면 한수가 아니므로 1을 반환
 			if (d != target.charAt(i+1) - target.charAt(i)) {
-				return 1;
+				return false;
 			}
 		}
-		// for문을 통과했다면 n은 한수이므로 0을 리턴
-		return 0;
+		// for문을 통과했다면 n은 한수이므로 true를 리턴
+		return true;
 	}
 }
