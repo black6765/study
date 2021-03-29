ae5fbf86e35502963c05c78d5ab640f1a696ad96
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2021-03-29 15:20:44 +0900

    Update 10250.java

--- java/기초 수학 1/10250.java
+++ java/기초 수학 1/10250.java
@@ -1,13 +1,17 @@
 import java.io.*;
 import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         int H, W, N;
         int X, Y;
+
+        StringTokenizer st;
+
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
             H = Integer.parseInt(st.nextToken());
@@ -21,10 +25,10 @@ public class Main {
                 Y = H;
                 X -= 1;
             }
-
             bw.write(String.format("%d%02d", Y, X));
             bw.newLine();
         }
+
         bw.flush();
         bw.close();
         br.close();
