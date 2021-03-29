d7d7ca7397c97c39aa6b443b504e49c5fc6a9fa6
Kim Cheong-Jun <67176669+black6765@users.noreply.github.com>
2021-03-30 02:36:09 +0900

    Create 2775.java

---
+++ java/기초 수학 1/2775.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        int k, n;
+        int[][] arr = new int[15][15];
+
+        for (int i = 0; i < 15; i++) {
+            arr[0][i] = i;
+            arr[i][0] = 0;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15; j++) {
+                arr[i][j] = arr[i-1][j] + arr[i][j-1];
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            k = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
+            bw.write(String.valueOf(arr[k][n]));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
