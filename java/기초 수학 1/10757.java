c1fbcd58fd34b002716b0a51d0c5e89e4ccf5abf
black6765 <67176669+black6765@users.noreply.github.com>
2021-04-05 23:57:54 +0900

    Update 10757.java

--- java/기초 수학 1/10757.java
+++ java/기초 수학 1/10757.java
@@ -8,11 +8,14 @@ public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		// StringTokenizer로 공백을 기준으로 구분
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		// 입력받은 두 수를 저장
 		BigInteger A = new BigInteger(st.nextToken());
 		BigInteger B = new BigInteger(st.nextToken());
 		
+		// A+B의 결과를 출력(add 메소드)
 		System.out.println(A.add(B));
 		
 		br.close();
