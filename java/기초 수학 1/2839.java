88f5a255bee976a2344a4729e2b2af1188de2520
black6765 <67176669+black6765@users.noreply.github.com>
2021-04-06 17:43:04 +0900

    Update 2839.java

--- java/기초 수학 1/2839.java
+++ java/기초 수학 1/2839.java
@@ -8,7 +8,9 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
+		// -1이 출력되는 경우는 4 또는 7
 		if (N == 4 || N == 7) System.out.println("-1");
+		// 4 또는 7 이외의 모든 수는 아래의 조건문에 해당
 		else if (N % 5 == 0) System.out.println(N / 5);
 		else if (N % 5 == 1 || N % 5 == 3) System.out.println(N / 5 + 1);
 		else if (N % 5 == 2 || N % 5 == 4) System.out.println(N / 5 + 2);
