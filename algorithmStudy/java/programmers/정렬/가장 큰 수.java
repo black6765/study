c76aea02559c274d268cdcebc199268417e7fa53
black6765 <67176669+black6765@users.noreply.github.com>
2021-05-26 01:36:06 +0900

    Create 가장 큰 수.java

---
+++ algorithmStudy/java/programmers/정렬/가장 큰 수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String [] arr = new String[numbers.length];
+        
+        for (int i = 0 ; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return -(a+b).compareTo(b+a);
+   
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String i : arr) {
+            sb.append(i);
+        }
+        
+        if (sb.charAt(0) == '0') {
+            sb.delete(0, sb.length());
+            sb.append("0");
+        }
+        
+        answer = String.valueOf(sb);
+        
+        return answer;
+    }
+}
