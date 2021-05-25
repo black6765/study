056ebda7b2faeaafd0c0cd88505e56a05ff38abb
black6765 <67176669+black6765@users.noreply.github.com>
2021-05-26 01:46:45 +0900

    Update 가장 큰 수.java

--- algorithmStudy/java/programmers/정렬/가장 큰 수.java
+++ algorithmStudy/java/programmers/정렬/가장 큰 수.java
@@ -34,3 +34,42 @@ class Solution {
         return answer;
     }
 }
+
+// List 컬렉션 이용
+//import java.util.*;
+//
+//class Solution {
+//    public String solution(int[] numbers) {
+//        String answer = "";
+//        
+//        List<String> arr = new ArrayList<>();
+//        
+//        for (int i = 0 ; i < numbers.length; i++) {
+//            arr.add(String.valueOf(numbers[i]));
+//        }
+//
+//        Collections.sort(arr, new Comparator<String>() {
+//            @Override
+//            public int compare(String a, String b) {
+//                return -(a+b).compareTo(b+a);
+//   
+//            }
+//        });
+//        
+//        
+//        StringBuilder sb = new StringBuilder();
+//        
+//        for(String i : arr) {
+//            sb.append(i);
+//        }
+//        
+//        if (sb.charAt(0) == '0') {
+//            sb.delete(0, sb.length());
+//            sb.append("0");
+//        }
+//        
+//        answer = String.valueOf(sb);
+//        
+//         return answer;
+//    }
+//}
