9d0e32e0fe48b074a01a95f703c84d81f3c7bbb4
black6765 <67176669+black6765@users.noreply.github.com>
2021-11-04 21:40:48 +0900

    Update 1.java

--- algorithmStudy/java/boj/1.java
+++ algorithmStudy/java/boj/1.java
@@ -3,15 +3,6 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-		
-		while(N % 2 == 0) {
-			N /= 2;
-		}
-		
-		if(N == 1) System.out.println("1");
-		else System.out.println("0");
 	}
 }
